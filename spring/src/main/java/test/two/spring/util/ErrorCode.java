5f4d3373769c666954701c938af2aecfa352accc
Aimer <1428616906@qq.com>
2019-01-06 17:39:48 +0800

    是

--- spring/src/main/java/test/two/spring/util/ErrorCode.java
+++
@@ -1,7 +0,0 @@
-package test.two.spring.util;
-
-public class ErrorCode {
-    public final static String SUCCESS = "000";
-    public final static String WRONG_PWD = "001";
-    public final static String WRONG_NAME = "002";
-}
