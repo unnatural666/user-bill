c9b91f1abecff5a03901dd4064d44197de642a8c
zhangxiansheng123 <767743430@qq.com>
2019-01-11 20:27:18 +0800

    jj

--- bill/src/views/Lobby.vue
+++ bill/src/views/Lobby.vue
@@ -1,7 +1,15 @@
 <template>
 
 <div style="width: 100% ;height: auto;margin-top:80px ">
-  <div>
+  <div style="width: 800px;height: 260px;">
+    <div class="dormitory">
+      <!--<div class="searchWord">-->
+        <!--<div style="display: inline-block"> 搜索：</div>-->
+        <!--<el-input v-model="search" style="display: inline-block;width: 200px"-->
+                  <!--placeholder="请输入搜索内容">-->
+        <!--</el-input>-->
+      <!--</div>-->
+      <div>
     <table style=" line-height: 35px;width: 900px; margin-left: 40px">
       <tr>
         <th style="padding-left: 0px ;font-size: 16px"><i class="el-icon-star-on"></i> 承兑对象</th>
@@ -10,14 +18,7 @@
         <td>国股</td>
         <td>三农</td>
         <td>财票</td>
-        <td width="120px"><el-input
-          type="textarea"
-          autosize
-          placeholder="请输入承兑人"
-          resize='none'
 
-        >
-        </el-input></td>
       </tr>
       <tr>
         <th style="padding-left: 0px ;font-size: 16px"><i class="el-icon-goods"></i> 票面金额</th>
@@ -26,20 +27,16 @@
         <td>100万以上</td>
         <td>500万以上</td>
         <td>财票</td>
-        <td width="150px"><el-input
-          type="textarea"
-          resize='none'
-          autosize
-          placeholder="最低金额"
-          >
-        </el-input></td>---
-        <td width="150px"><el-input
-          type="textarea"
-          autosize
-          resize='none'
-          placeholder="最高金额"
-         >
-        </el-input></td>
+        <td width="150px">
+
+        </td>
+        <!--<td width="150px"><el-input-->
+          <!--type="textarea"-->
+          <!--autosize-->
+          <!--resize='none'-->
+          <!--placeholder="最高金额"-->
+         <!--&gt;-->
+        <!--</el-input></td>-->
 
       </tr>
       <tr>
@@ -49,19 +46,12 @@
         <td>190-180天</td>
         <td>180-360天</td>
         <td>180-360天</td>
-
-        <td width="150px"><el-input
-          type="textarea"
-          autosize
-          resize='none'
-          placeholder="输入天数"
-         >
-        </el-input></td>
       </tr>
       <tr >
         <th style="padding-left: 0px ;font-size: 16px"><i class="el-icon-document"></i> 有无瑕疵</th>
         <td>有瑕疵</td>
         <td>无瑕疵</td>
+
 
       </tr>
       <tr >
@@ -71,29 +61,18 @@
         <td>优秀</td>
 
       </tr>
-      <tr>
-        <th style="padding-left: 0px ;font-size: 16px"><i class="el-icon-zoom-out"></i> 最低票价</th>
-
-
-        <td width="80px"><el-input
-          type="textarea"
-          autosize
-          resize='none'
-          placeholder="年利率"
-         >
-        </el-input></td>
-        <td width="80px"><el-input
-          type="textarea"
-          autosize
-          resize='none'
-          placeholder="手续费"
-         >
-        </el-input></td>
-      </tr>
     </table>
+      </div>
+      <div style="margin-left: 60px">
+        <el-input v-model="search" style="display: inline-block;width: 500px" placeholder="请输入票据金额或承兑人或到期时间">
+        </el-input>
+      </div>
   </div>
- <div style="height: 400px;width: 100%; ">  <el-table
-   :data="tableData"
+  </div>
+ <div style="height: auto;width: 100%; " class="dormitoryData">
+
+   <el-table
+     :data="tables.slice((pageIndex-1)*pageSize,pageIndex*pageSize)"
    style="width: 100%">
    <el-table-column
      label="票据类型"
@@ -107,7 +86,7 @@
      width="180" >
      <template slot-scope="scope">
 
-       <span style="margin-left: 10px">{{ scope.row.eename }}</span>
+       <span style="margin-left: 10px" v-html="format(scope.row.eename)"></span>
      </template>
    </el-table-column>
    <el-table-column
@@ -115,7 +94,7 @@
      width="180">
      <template slot-scope="scope">
 
-       <span style="margin-left: 10px">{{ scope.row.money }}</span>
+       <span style="margin-left: 10px" v-html="format(scope.row.money)"></span>
      </template>
    </el-table-column>
    <el-table-column
@@ -123,7 +102,7 @@
      width="180">
      <template slot-scope="scope">
 
-       <span style="margin-left: 10px">{{ scope.row.billtime }}</span>
+       <span style="margin-left: 10px" v-html="format( scope.row.billtime )"></span>
      </template>
    </el-table-column>
    <el-table-column
@@ -131,7 +110,7 @@
      width="180">
      <template slot-scope="scope">
 
-       <span style="margin-left: 10px">{{ scope.row.want }}</span>
+       <span style="margin-left: 10px"  v-html="format( scope.row.want )"></span>
      </template>
    </el-table-column>
 
@@ -148,20 +127,24 @@
    </el-table-column>
  </el-table>
    <el-pagination
-   :page-sizes="[100, 200, 300, 400]"
-   :page-size="100"
-   style="margin-left: 600px"
-   layout="total, sizes, prev, pager, next, jumper"
-   :total="400">
+     @size-change="handleSizeChange"
+     @current-change="handleCurrentChange"
+     :current-page="pageIndex"
+     :page-sizes="[2, 3, 4, 5]"
+     :page-size="pageSize"
+     layout="total, sizes, prev, pager, next, jumper"
+     :total="total">
    </el-pagination>
+
    <div class="repair">
      <el-dialog title="报价" :visible.sync="dialogFormVisible" >
       <div style="width: 560px;height: 400px;">
 
          <div style="text-align: center"> <img :src="billinfo.path" alt="" style="width: 350px;height: 200px;margin-top: -20px;margin-left: -22px"></div>
-        <div>
+        <div >
+          <h4 style="text-align: center">承兑对象:{{billinfo.eename}}</h4>
           <table class="gridtable">
-            <tr><td ><h3>承兑对象:{{billinfo.eename}}</h3></td><td ></td></tr>
+
             <tr><td >票据金额:{{billinfo.money}}</td><td >票据账号:{{billinfo.billname}}</td></tr>
             <tr><td>票据瑕疵:{{billinfo.flaw}}</td><td>期望价格:{{billinfo.want}}</td></tr>
             <tr><td>背书次数:{{billinfo.endorse}}</td><td>到期时间:{{billinfo.billtime}}</td></tr>
@@ -188,57 +171,102 @@
     name: 'AboutUs',
     data() {
       return {
+        pageSize: 2,
+        pageIndex: 1,
+        search: '',
         dialogTableVisible: false,
         dialogFormVisible: false,
         formLabelWidth: '120px',
-        billinfo:'',
+        billinfo: '',
         tableData: [{
-          billname:'',
+          billname: '',
           eename: '',
           money: '',
           billtime: '',
           want: '',
-          endorse:''
+          endorse: ''
         }],
 
       }
     },
+    computed: {
+      // 模糊搜索
+      tables () {
+        const search = this.search
+        if (search) {
+          return this.tableData.filter(dataNews => {
+            return Object.keys(dataNews).some(key => {
+              return String(dataNews[key]).toLowerCase().indexOf(search) > -1
+            })
+          })
+        }
+        return this.tableData
+      },
+      // 总条数
+      total () {
+        return this.tables.length
+      }
+    },
+
+
     created() {
       this.axios.post('http://localhost:8081/show')
         .then(res => {
-          this.tableData=res.data
+          this.tableData = res.data
           // console.log(this.tableData);
         })
         .catch(error => {
 
         })
     },
+    watch: {
+      // 检测表格数据过滤变化，自动跳到第一页
+      tables () {
+        this.pageIndex = 1
+      }
+    },
     methods: {
+      format (val) {
+        val = val.toString()
+        if (val.indexOf(this.search) !== -1 && this.search !== '') {
+          return val.replace(this.search, '<font color="red">' + this.search + '</font>')
+        } else {
+          return val
+        }
+      },
 
+      handleSizeChange (val) {
+        this.pageSize = val
+      },
+      handleCurrentChange (val) {
+        this.pageIndex = val
+      },
+        bill(index, row) {
+          let con = this.dormitory
+          // let connt=con[index].billname
+          this.dialogFormVisible = true
+          let config = {
+            headers: {"Content-Type": "application/json;charset=UTF-8"}
+          };
+          this.axios.post('http://localhost:8081/contend', con[index].billname, config)
+            .then(res => {
+              this.billinfo = res.data
+
+            })
+            .catch(error => {
+
+            })
+
+        },
+
+      },
       // handleDelete(index, row) {
       //   console.log(index, row);
       //   alert("hello")
       // },
-      bill(index, row){
-          let con=this.tableData
-       // let connt=con[index].billname
-        this.dialogFormVisible = true
-        let config={
-          headers: { "Content-Type": "application/json;charset=UTF-8" }
-        };
-        this.axios.post('http://localhost:8081/contend',con[index].billname,config)
-          .then(res => {
-            this.billinfo=res.data
 
-          })
-          .catch(error => {
-
-          })
-
-      },
     }
 
-  }
 
 </script>
 
